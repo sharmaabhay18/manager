86ff96bc823ac933eef607aba18398ca0d249c5f
sharmaabhay18 <sharmaabhay47@gmail.com>
2018-08-18 09:04:50 -0700

    Update App.js

--- App.js
+++ App.js
@@ -11,12 +11,7 @@ componentWillMount() {
    const firebase = require('firebase');
    
   const config = {
-  apiKey: 'AIzaSyB_DRPyFdMsUWftwDUdtpjFz8_1gt1AZes',
-  authDomain: 'manager-c3d  6e.firebaseapp.com',
-  databaseURL: 'https://manager-c3d6e.firebaseio.com',
-  projectId: 'manager-c3d6e',
-  storageBucket: 'manager-c3d6e.appspot.com',
-  messagingSenderId: '236833720569'
+
 };
 firebase.initializeApp(config);
 }
